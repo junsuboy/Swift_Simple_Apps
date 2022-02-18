87487ebd5aa01a892b83c50938798fb980dd2a1e
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-02-19 00:56:32 +0900

    버튼 클릭시 Alpha값 변경

---
+++ LEDBoard/LEDBoard/SettingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by Junsu Jang on 2022/02/18.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tabTextColorButton(_ sender: UIButton) {
+        if sender == self.yellowButton {
+            self.changeTextColor(color: .yellow)
+        } else if sender == self.purpleButton {
+            self.changeTextColor(color: .purple)
+        } else if sender == self.greenButton {
+            self.changeTextColor(color: .green)
+        }
+    }
+    
+    @IBAction func tabBackgroundColorButton(_ sender: UIButton) {
+        if sender == self.blackButton {
+            self.changeBackgroundColorButton(color: .black)
+        } else if sender == self.blueButton {
+            self.changeBackgroundColorButton(color: .blue)
+        } else if sender == self.orangeButton {
+            self.changeBackgroundColorButton(color: .orange)
+        }
+    }
+    
+    @IBAction func tabSaveButton(_ sender: UIButton) {
+    }
+    
+    private func changeTextColor(color: UIColor) {
+        self.yellowButton.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleButton.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenButton.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    
+    private func changeBackgroundColorButton(color: UIColor) {
+        self.blackButton.alpha = color == UIColor.black ? 1 : 0.2
+        self.blueButton.alpha = color == UIColor.blue ? 1 : 0.2
+        self.orangeButton.alpha = color == UIColor.orange ? 1 : 0.2
+    }
+}
