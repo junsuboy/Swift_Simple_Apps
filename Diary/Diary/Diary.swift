92a76369983c67b987a5d61d78eb247a34b25465
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-02-22 23:48:25 +0900

    일기 등록 기능 구현

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Junsu Jang on 2022/02/22.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
