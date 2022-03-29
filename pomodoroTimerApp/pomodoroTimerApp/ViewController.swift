1faf1c5a5a5fd67133fd00aaf776f9db00a1096a
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-03-29 11:47:34 +0900

    UI 구성 완료

--- pomodoroTimerApp/pomodoroTimerApp/ViewController.swift
+++ pomodoroTimerApp/pomodoroTimerApp/ViewController.swift
@@ -7,15 +7,59 @@
 
 import UIKit
 
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    var duration = 60
+    var timerStatus: TimerStatus = .end
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureToggleButton()
     }
+    
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progressView.isHidden = isHidden
+    }
+    
+    func configureToggleButton() {
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+    
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+    }
+    
 
-
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+        switch self.timerStatus {
+        case .end:
+            self.timerStatus = .start
+            self.setTimerInfoViewVisible(isHidden: false)
+            self.datePicker.isHidden = true
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+        case .start:
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false
+        
+        default:
+            break
+        }
+    }
 }
 
