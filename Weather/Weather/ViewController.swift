ca30ac1e6a9a338264805504f1db3020507c7760
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-02 02:23:25 +0900

    날씨 API 받아오기 기능구현

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -27,6 +27,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=ba273bc91e0c46892b99581ed25a4dcf") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
     }
     
 }
