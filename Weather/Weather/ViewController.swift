bd688cf228849dec7e1b1e0dd9ba8fad027ce3db
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-01 23:58:19 +0900

    outlet 변수 등록

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var WeatherDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    @IBOutlet weak var minTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+    }
+    
 }
 
---
+++ Weather/Weather/WeatherInformation.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherInformation.swift
+//  Weather
+//
+//  Created by Junsu Jang on 2022/04/01.
+//
+
+import Foundation
+
+struct WeatherInformation: Codable {
+    
+}
