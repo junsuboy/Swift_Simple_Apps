bcebe4a706644ab0257ad4073954263c787ce046
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-02 02:48:41 +0900

    날씨 API로 정보 가져와 화면에 표시

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by Junsu Jang on 2022/04/02.
+//
+
+import Foundation
+
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var maxTempLabel: UILabel!
     
     @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +34,27 @@ class ViewController: UIViewController {
         }
     }
     
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.WeatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))℃"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))℃"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))℃"
+    }
+    
     func getCurrentWeather(cityName: String) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=ba273bc91e0c46892b99581ed25a4dcf") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder()
-            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
-            debugPrint(weatherInformation)
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+            DispatchQueue.main.async {
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
         }.resume()
     }
     
