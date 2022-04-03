cdf24f72d9eaa8f4fd118d62471ec9bb8c6c09b5
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-03 18:55:50 +0900

    API 호출부분 구현 완료

--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -17,9 +19,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        })
     }
-
-
+    
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "Qr5MELz2BOgSfuq8sNixtk1PVbRToF69l"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+    }
 }
 
